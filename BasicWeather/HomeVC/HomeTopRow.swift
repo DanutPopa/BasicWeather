34516cdddd999f2b75dffbac1d7e8a6c0e57d11b
DanutPopa <popa.ion.danut@gmail.com>
2025-06-17 19:53:41 +0300

    Making the UI for the HomeTopRow cell.

--- BasicWeather/HomeVC/HomeTopRow.swift
+++ BasicWeather/HomeVC/HomeTopRow.swift
@@ -9,13 +9,23 @@ import UIKit
 
 class HomeTopRow: UITableViewCell {
     static let id = "HomeTopRow"
-
+    
+    @IBOutlet private weak var img: UIImageView!
+    @IBOutlet private weak var temperatureLabel: UILabel!
+    @IBOutlet private weak var locationLabel: UILabel!
+    @IBOutlet private weak var descriptionLabel: UILabel!
+    @IBOutlet private weak var highLowLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
+    }
+    
+    func configure() {
+        
     }
 
 }
