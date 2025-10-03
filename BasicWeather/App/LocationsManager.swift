9788ed4a17706644180678d8acff428e9ba0ad68
Danut Popa <popa.ion.danut@gmail.com>
2025-10-03 12:38:48 +0300

    Saving locations to User Defaults.

--- BasicWeather/App/LocationsManager.swift
+++ BasicWeather/App/LocationsManager.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+class LocationsManager {
+    static let shared = LocationsManager()
+    
+    private init() {}
+    
+    private let defaults = UserDefaults.standard
+    
+    private var locations: [SearchLocation] = []
+    
+    func getLocations() -> [SearchLocation] {
+        locations
+    }
+    
+    func appendAndSave(_ location: SearchLocation) {
+        locations.append(location)
+        saveLocation()
+    }
+    
+    private func saveLocation() {
+        do {
+            var encoded: [Data] = []
+            var encoder = JSONEncoder()
+            for location in locations {
+                let data = try encoder.encode(location)
+                encoded.append(data)
+            }
+            defaults.set(encoded, forKey: "Locations")
+        } catch {
+            print(error)
+        }
+    }
+}
